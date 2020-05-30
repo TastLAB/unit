b33d244bd0e5c2662df986a3134a0fde9111e5de
Luna <nightly@outlook.kr>
2020-05-30 17:16:30 +0900

    key value update
    Change h_id variable from Object to Map

--- unit.js
+++ unit.js
@@ -1,7 +1,7 @@
 const cluster = require('cluster');
 var query_index = 0, h_gc;
 const query_index_max = 0xffffff;
-const h_id = {};
+const h_id = new Map();
 
 async function send(value, handle, option){
 	process.send(value, handle, option);
@@ -18,14 +18,14 @@ function start_gc(){
 	if(h_gc) return;
 
 	h_gc = setInterval(() => {
-		for(const n in h_id){
+		for(const [a,b] of h_id){
 			if(--h_id[n].timeout <= 0){
 				h_id[n].result_value(null);
 				delete h_id[n];
 			}
 		}
 
-		for(const n in h_id) return;
+		if( h_id.size > 0 ) return;
 
 		clearInterval(h_gc);
 		h_gc = null;
@@ -37,29 +37,29 @@ const m = module.exports = {
 	deploy:(_unit, _processes, ... _host)=>{
 		if(!_processes) _processes = require('os').cpus().length;
 
-		const w_unit = new Array((_host?_host.length:0) + _unit.length*_processes);
+		const len_unit = _unit.length, len_host = _host.length;
+		const w_unit = new Array((_host?_host.length:0) + len_unit*_processes);
 		var i = 0;
-		const lenh = _host.length, lend = _unit.length;
 		if(_host){
-			for(; i<lenh; i++) w_unit_init(_host[i], i, {host_count:lenh, unit_count:lend});
+			for(; i<len_host; i++) w_unit_init(_host[i], i, {host_count:len_host, unit_count:len_unit, processes:_processes});
 		}
-		const len = i + lend*_processes;
-		for(; i < len; i+=lend){
-			const len = i+lend;
-			for(var j=i,k=0; j<len; j++,k++) w_unit_init(_unit[k], j, {index_group:(i-lenh)/lenh});
+		const len = i + len_unit*_processes;
+		for(var index_group=0; i < len; index_group++,i+=len_unit){
+			const len = i+len_unit;
+			for(var j=i,k=0; j<len; j++,k++) w_unit_init(_unit[k], j, {index_group});
 		}
 
-		function w_unit_init(__type, i, env){
-console.log('init',__type, i);
-			const a = w_unit[i] = cluster.fork({ type:__type, index:i.toString(), ...env });
+		function w_unit_init(type, index, env){
+console.log('init',type, index);
+			const a = w_unit[index] = cluster.fork({ type, index, ...env });
 			a.on('message', (query, handle)=>{
 				if(w_unit[query.to]) w_unit[query.to].send(query, handle);
 				else a.send(query);
 			});
 			a.on('exit',(code, signal)=>{
-console.log('exit',__type, i);
-				w_unit[i] = null;
-				w_unit_init(__type, i, env);
+console.log('exit',type, index);
+				w_unit[index] = null;
+				w_unit_init(type, index, env);
 			});
 		}
 
@@ -83,13 +83,13 @@ console.log('exit',__type, i);
 						return new Promise((resolve, reject)=>{
 							start_gc();
 
-							h_id[id] = {
+							h_id.set(id, {
 								timeout:timeout|5,
 								result_value:(value)=>{
-									delete h_id[id];
+									h_id.delete(id);
 									resolve(value)
 								}
-							};
+							});
 						})
 					}
 				};
@@ -101,11 +101,14 @@ console.log('exit',__type, i);
 
 		if('host_count' in process.env) z.host_count = process.env.host_count|0;
 		if('unit_count' in process.env) z.unit_count = process.env.unit_count|0;
+		if('processes' in process.env) z.processes = process.env.processes|0;
+
 		if('index_group' in process.env) z.index_group = process.env.index_group|0;
 
 		process.on('message',async(query, handle)=>{
 			if(query.id != 0 && query.index == query.to){
-				if(query.id in h_id)h_id[query.id].result_value(query.value);
+				const a = h_id.get(query.id);
+				if( a ) a.result_value(query.value);
 			}else if(listner){
 				try{
 					const result = await listner(query.value, handle);
